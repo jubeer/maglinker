e58d1794fcb9801b4fa95666a1396ef18fd7a499
jubeer <w.sobiak@yahoo.pl>
2016-03-02 18:57:17 +0100

    Init

--- test.php
+++ test.php
@@ -7,27 +7,34 @@ if (!isset($_POST['login']) || (!isset($_POST['pass']))) {
     exit();
 }
 
-require_once "dbconnect.php";
 
-if (db_connect()) {
+$connection = mysqli_connect('localhost','root','Q!1qW@2wE#3e','testowa');
+
+
+
+
+if($connection === false) {
+    echo "fail db_connect";
+    $error = db_error();
+    echo $error;
+
+} else {
     echo "success db_connect";
+
+    $rezultat = $connection->query("SELECT * FROM test");
 
     //$login = db_quote($_POST['login']);
     //$pass = $_POST['pass'];
 
-    $rows = db_select("SELECT * FROM testowa");
+    //$rows = db_select("SELECT * FROM testowa");
 
-    var_dump($rows);
+    var_dump($rezultat);
     //var_dump($login);
     //var_dump($pass);
 
 
     $error = db_error();
     echo $error;
-} else {
-    echo "fail db_connect";
-    $error = db_error();
-    echo $error;
 }
- echo "skipped db_connect()";
+echo "skipped db_connect()";
 ?>
