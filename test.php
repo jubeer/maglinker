dda7e1e3db3b9b44c7af9d6c5c5b83e8d91542dc
jubeer <w.sobiak@yahoo.pl>
2016-03-02 08:25:19 +0100

    Init

--- test.php
+++ test.php
@@ -11,6 +11,14 @@ require_once "dbconnect.php";
 
 if (db_connect()) {
     echo "success db_connect";
+
+    $login = db_quote($_POST['login']);
+    $pass = $_POST['pass'];
+
+    $rows = db_select("SELECT * FROM users WHERE username=$login");
+
+    var_dump($rows);
+
     $error = db_error();
     echo $error;
 } else {
